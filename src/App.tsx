b9197bea01ddf961ce3384e1466c353e3115a8f0
Jeff Chebul <jmchebul@jmchebul.com>
2024-06-18 16:11:33 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 
 import './App.css'
 import Navigation from './Components/Navigation';
 import Home from './Views/Home';
+import Footer from "./Components/Footer";
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
     
   )
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -18,7 +18,7 @@ export default function Navigation({}: Props) {
     </div>
   </div>
   <div className="navbar-center">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Jeff Chebul | Software Engineer</a>
   </div>
   <div className="navbar-end">
     <button className="btn btn-ghost btn-circle">
