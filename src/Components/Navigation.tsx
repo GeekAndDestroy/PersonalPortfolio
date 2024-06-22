9084bbb86b43fea658aaf807da1bdcbbdbd73c1a
Jeff Chebul <jmchebul@jmchebul.com>
2024-06-22 11:07:01 -0500

    themes added

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -21,15 +21,19 @@ export default function Navigation({}: Props) {
     <a className="btn btn-ghost text-xl">Jeff Chebul | Software Engineer</a>
   </div>
   <div className="navbar-end">
-    <button className="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
-      </div>
-    </button>
+  <div className="dropdown">
+  <div tabIndex={0} role="button" className="btn m-1">
+    Theme
+    <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+  </div>
+  <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dark" value="dark"/></li>
+    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Light" value="light"/></li>
+    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Cyberpunk" value="cyberpunk"/></li>
+    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Dracula" value="dracula"/></li>
+    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Acid" value="acid"/></li>
+  </ul>
+</div>
   </div>
 </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+      extend: {},
   },
-  plugins: [require('daisyui'),],
-}
-
+  daisyui: {
+      themes: [
+        "cyberpunk",
+        "light",
+        "dracula",
+        "acid",
+          {
+              dark: {
+                  primary: "#00ff00",
+                  secondary: "#E200E6",
+                  accent: "#ffff00",
+                  neutral: "#E200E6",
+                  "base-100": "#21325E",
+                  info: "#0000ff",
+                  success: "#00ff00",
+                  warning: "#00ff00",
+                  error: "#ff0000",
+                  "--glass-opacity": "5%",
+                  "--glass-reflex-opacity": "3%",
+                  "--glass-blur": "25px",
+              },
+              // light: {
+              //   "primary": "#00ff00",
+              //   "secondary": "#E200E6",
+              //   "accent": "#ffff00",
+              //   "neutral": "#E20026",
+              //   "base-100": "#ffffff",
+              //   "info": "#0000ff",
+              //   "success": "#00ff00",
+              //   "warning": "#00ff00",
+              //   "error": "#ff0000",
+              //   "--glass-opacity": "5%",
+              //   "--glass-reflex-opacity": "3%",
+              //   "--glass-blur": "25px",
+              //   "#file-upload-button": {
+              //     "background-color": "#E200E6",
+              //   },
+                        //  },
+          },
+      ],
+  },
+  plugins: [require("daisyui")],
+};
