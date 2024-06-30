b27aa2222612c4d4ae8725d66daa2b18149a62cc
Jeff Chebul <jmchebul@jmchebul.com>
2024-06-29 21:14:22 -0500

    save before testing forced install on react-motion

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
 type HomeProps = {}
 
@@ -50,12 +51,16 @@ export default function Home({}: HomeProps) {
   <div className="hero-content flex-col lg:flex-row ">
     {/* <img src="../../src/assets/images/headshot/headshot01.jpg" className="max-w-sm rounded-lg shadow-2xl" /> */}
     {/* <div className="size-fit"><Headshot /></div> */}
+    
     <img id="myPicture" src={currentImage} style={{height:576, width:384}} alt="Random Headshot" />
+    
     <div className="ml-6">
+      {/* <Slide direction={'right'}> */}
       <h1 className="text-5xl font-bold">WELCOME!</h1>
       <p className="py-6">Jeff Chebul is a full stack engineer with a knack for front end development, enjoying the challenge of turning design ideas into smooth user experiences. Jeff excels at solving problems, managing multiple projects, and building strong client relationships. He is also passionate about animal welfare and mental health advocacy. Quick to learn new languages and technologies, Jeff consistently aims to deliver innovative solutions.</p>
       {/* <p className="py-6">Hi, I'm Jeff Chebul! As a full stack engineer with a knack for front end development, I enjoy turning design ideas into smooth user experiences. I excel at solving problems, managing multiple projects, and building great client relationships. I’m also passionate about animal welfare and mental health advocacy. I’m quick to learn new languages and tech, always aiming to deliver innovative solutions.</p> */}
       {/* <button className="btn btn-primary">Get Started</button> */}
+      {/* </Slide> */}
     </div>
   </div>
 </div>
