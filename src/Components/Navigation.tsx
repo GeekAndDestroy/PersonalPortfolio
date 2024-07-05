bf6303e898364ed143a9da3473be68bc4dd587f6
Jeff Chebul <jmchebul@jmchebul.com>
2024-07-05 15:08:50 -0500

    disabled cointact link in nav

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -22,14 +22,14 @@ export default function Navigation({}: Props) {
         <li><a href="/">Home</a></li>
         <li><a href="/projects">Projects</a></li>
         <li><a href="/experience">Experience</a></li>
-        <li><a href="/contact">Contact</a></li>
+        {/* <li><a href="/contact">Contact</a></li> */}
       </ul>
     </div>
   </div>
   <div className="navbar-center">
     {/* <a className="btn btn-ghost text-xl">Jeff Chebul | Full Stack Engineer</a> */}
     
-    <svg width="350" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
+    <svg width="300" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
 	 viewBox="280 500 1440 395" className="fill-current relative top-5">
 <g>
 	<path d="M587.21,894.96c-1.33,0-2.56-0.45-3.7-1.35c-1.14-0.9-1.84-2.26-2.08-4.06c-1.56-26.83-10.65-49.53-27.25-68.11
